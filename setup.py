30082d8a565591ed767f5f917b7b2d928521e31d
UniNilsBrehm <hd280kilxtc>
2025-04-23 11:19:57 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="roibaview",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "PyQt6",
+        "pyqtgraph",
+        "numpy",
+        "pandas",
+        "scipy",
+        "h5py",
+        "tifftools",
+        "tifffile",
+        "opencv-python",
+        "ffmpy",
+        "roifile"
+    ],
+    entry_points={
+        "gui_scripts": [
+            "roibaview = roibaview.main:main",
+        ],
+    },
+    python_requires=">=3.7",
+    author="Nils",
+    author_email="your.email@example.com",
+    description="A PyQt6-based application for ROI visualization.",
+    url="https://github.com/UniNilsBrehm/RoiBaView",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
